6cba21195cca645cc0ab0e25a140b3b92e82e025
ric7x <ricardorosario89@gmail.com>
2016-11-11 20:58:05 -0400

    completaE

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -57,7 +57,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(125, 164);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
